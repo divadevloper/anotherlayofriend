2147c5ca84dbb654f6762087c438ecd9d4ae1e2c
Danicode <ariyoesther2009@gmail.com>
2025-02-11 09:39:58 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { MessageComponent } from './components/message/message.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'message/:id', component: MessageComponent },
+];
---
+++ src/app/components/message/message.component.ts
@@ -0,0 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+// import {CommonModule}
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css',
+})
+export class MessageComponent {
+  message: string = '';
+
+  constructor(private route: ActivatedRoute) {}
+
+  images: string[] = [
+    'assets/imageone.jpg',
+    'assets/imagetwo.jpg',
+    'assets/imagethree.jpg',
+  ];
+  currentIndex = 0;
+
+  startCarousel(): void {
+    setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 3000);
+  }
+
+  ngOnInit(): void {
+    this.startCarousel();
+    const messageId = this.route.snapshot.paramMap.get('id');
+    if (messageId === 'love123') {
+      this.message = 'To the LOVE OF MY LIFE🥰';
+    } else {
+      this.message = 'Welcome to your Valentine’s message!';
+    }
+  }
+}
